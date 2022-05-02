145792bbc5044463dad87698c10aa112cc39fcda
Joël <joel.jon@moix.me>
2022-05-02 09:43:34 +0200

    Importo le librerie esterne direttamente con CDN

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+bo<?php
 
 /**
  * index.php    file principale per il caricamento del programma 
@@ -28,18 +28,18 @@ if (!utente::isLogged()) {
 <head>
     <meta charset="utf-8">
     <!-- Importo JQuery e JQuery di bootstrap-->
-    <script src="lib/JQuery/JQuery.js"></script>
-    <script src="lib/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
     <!-- Importo Bootbox -->
-    <script src="lib/bootbox/bootbox.all.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.js" integrity="sha512-OMYI9iOelB12PWdWHfU6XouDuUvszFZEywO4W9KFJGP3aj/nP5UECd5ctMqRm+/9Qk3oOFqhbXVi6cJAqlAUuA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Importo bootstrap -->
-    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
     <!-- Importo Icone -->
-    <link rel="stylesheet" href="/lib/fontawesome/css/all.css">
-    <link rel="stylesheet" href="/lib/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <!-- importo custom Js -->
     <?php echo $ret['js']; ?>
