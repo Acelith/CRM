33e882b24b050a9ba7cd40c22f858bdffcbbcd54
Acelith <joel.jon@moix.me>
2022-05-04 16:25:20 +0200

    Aggiornato le funzioni globali di Javascript per poter eliminare un parametro specifico dall'URL

--- functions/functions.js
+++ functions/functions.js
@@ -1,76 +1,97 @@
 /**
- * functions.js: Funzioni javascript globali 
+ * functions.js: Funzioni javascript globali
  *
- * @author Joël Moix  
+ * @author Joël Moix
  */
 
 /**
  * Chiamata Ajax per eseguire il logout
  */
-function logout(){
-        $.ajax({
-            type: "POST",
-            url: "/functions/ajax_services.php",
-            data: {
-                cmd: "logout",
-            },
-            success: function (text) {
-              try{
-                var objVal;
-                
-                objVal = JSON.parse(text);
-                if (objVal.ajax_result !== "ok") {
-                    alert(objVal.ajax_result + " " +objVal.error);
-                    return false;
-                } else {
-                    location.reload(true);
-                } 
-              } catch (error) {
-                  alert("Errore: " + error + " " + text);
-              }
-                                
-            },
-        });
+function logout() {
+  $.ajax({
+    type: "POST",
+    url: "/functions/ajax_services.php",
+    data: {
+      cmd: "logout",
+    },
+    success: function (text) {
+      try {
+        var objVal;
+
+        objVal = JSON.parse(text);
+        if (objVal.ajax_result !== "ok") {
+          alert(objVal.ajax_result + " " + objVal.error);
+          return false;
+        } else {
+          location.reload(true);
+        }
+      } catch (error) {
+        alert("Errore: " + error + " " + text);
+      }
+    },
+  });
 }
 
 /**
  * Funzione per modificare i parametri Get del modulo
- * 
+ *
  * @param p_id      id del modulo nuovo
  */
-function changeModule(p_id){
-    changeParam('m', p_id);
+function changeModule(p_id) {
+  changeParam("m", p_id);
 }
 
 /**
-* Cambia il valore di un parametro. Nel caso il parametro non esista lo aggiunge 
-*
-* @param p_param_name   Nome del parametro da mettere nel GET per definirlo
-* @param p_param_value  valore da inserire nel get
-*
-*/
-function changeParam(p_param_name, p_param_value){
-    // Prendo l'url corrente 
-    var url = new URL(window.location.href);
+ * Cambia il valore di un parametro. Nel caso il parametro non esista lo aggiunge
+ *
+ * @param p_param_name   Nome del parametro da mettere nel GET per definirlo
+ * @param p_param_value  valore da inserire nel get
+ *
+ */
+function changeParam(p_param_name, p_param_value) {
+  // Prendo l'url corrente
+  var url = new URL(window.location.href);
 
-    var params = url.searchParams;
+  var params = url.searchParams;
 
-    // Imposto il nuovo parametro nell'url, nel caso non esistesse lo aggiunge
-    params.set(p_param_name, p_param_value);
-    
-    url.search = params.toString();
+  // Imposto il nuovo parametro nell'url, nel caso non esistesse lo aggiunge
+  params.set(p_param_name, p_param_value);
 
-    var new_url = url.toString();
-    // Ricarico la pagina con i parametri nuvoi
-    location.assign(new_url);
+  url.search = params.toString();
+
+  var new_url = url.toString();
+  // Ricarico la pagina con i parametri nuvoi
+  location.assign(new_url);
 }
+
+/**
+ * Cancella un parametro dall'URL
+ * 
+ * @param p_param_name nome del parametro da eliminare
+ */
+function delParam(p_param_name) {
+  // Prendo l'url corrente
+  var url = new URL(window.location.href);
+
+  var params = url.searchParams;
+
+  // Cancello il parametro dall'url
+  params.delete(p_param_name);
+
+  url.search = params.toString();
+
+  var new_url = url.toString();
+  // Ricarico la pagina con i parametri nuvoi
+  location.assign(new_url);
+}
+
 /**
  * Cancella tutti i parametri dall'url
  */
-function delParams(){
-    var url = new URL(window.location.href);
-    url.search = '';    
-    var new_url = url.toString();
-       
-    location.assign(new_url);
+function delParams() {
+  var url = new URL(window.location.href);
+  url.search = "";
+  var new_url = url.toString();
+
+  location.assign(new_url);
 }
