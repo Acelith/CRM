1cb4d9250f6ad56eb2061bd32ff33f03a7115ba5
Acelith <joel.jon@moix.me>
2022-05-12 09:03:11 +0200

    Bug fix

--- module/login/ajax_services.php
+++ module/login/ajax_services.php
@@ -87,6 +87,10 @@ function checkLogin($password, $email)
     }
 
     $usr = $sth->fetch(PDO::FETCH_OBJ);
+    if($usr == false){
+        return false;
+    }
+    
     if (password_verify($password, $usr->password)) {
         $_POST['usr_id'] = $usr->id;
         $_SESSION['id_utente'] = $usr->id;
